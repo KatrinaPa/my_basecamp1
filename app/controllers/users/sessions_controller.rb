ef4b7d7ef138fe19794e6622836a644cf57d09d1
Jurga <jurgak@gmail.com>
2025-02-26 23:30:32 +0200

    login / signup buttons hidden from the home page after sign in + improvements in error messages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,18 +1,24 @@
 class Users::SessionsController < Devise::SessionsController
-  # add custom code here if needed
-
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  rescue
-    flash[:alert] = if params[:user][:password].length < 6
-      "Password is too short (minimum is 6 characters)"
-    else
-      "Invalid email or password"
+    # Check if the email exists in the database first
+    user = User.find_by(email: params[:user][:email])
+
+    if user.nil?
+      flash[:alert] = "Email not found. Please check your email or sign up."
+      redirect_to new_user_session_path and return
     end
-    redirect_to new_user_session_path
+
+    if user.valid_password?(params[:user][:password])
+      # Successful authentication
+      self.resource = user
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      # Password is incorrect
+      flash[:alert] = "Incorrect password. Please try again."
+      redirect_to new_user_session_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_26_131842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_project_memberships_on_project_id"
-    t.index ["user_id", "project_id"], name: "index_project_memberships_on_user_id_and_project_id", unique: true
     t.index ["user_id"], name: "index_project_memberships_on_user_id"
   end
 
@@ -59,7 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_26_131842) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
+    t.integer "role"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
